705e03f8147c0269b91332d9af7dbafdab896710
hbfawaz112 <44267042+hbfawaz112@users.noreply.github.com>
2021-02-10 23:20:03 +0200

    first commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,81 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Hussein Fawaz', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Hussein B. Fawaz',
+  subtitle: 'A Full Stack Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'I am a Full stack web developer working with frontend technologies like HTML5-CSS3-Js and some javascript library like ReactJs , JQuery and backend technologies like PHP and ASP.NET core with some database like MySql and MongoDB',
+  paragraphTwo:
+    'I am also working with mobile development such as native android development using Java and cross platform mobile applications using flutter',
+  paragraphThree: 'Also working with desktop application development with java swing and javafx',
+  resume: 'https://drive.google.com/file/d/1VQ-gG2g_CuTk4gTxZj7m64z_CoaRPBK2/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'LFA-Grassroots.PNG',
+    title: 'LFA Grassroots Mobile Application',
+    info:
+      'This project is implemented using flutter technologie with mysql backend. Consist as an admin page (web page) to upload all data required to the grassroot football league (annoncment , matches , results , ranking,ect..) , and mobile application that work on both android and ios devices for the  users to see these data.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/hbfawaz112/LFA-Grassroots', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'twitter-monogoDB.png',
+    title: 'Twitter Clone',
+    info:
+      'A social media site like twitter to post , follow , like , comment , etc.. Developed using PHP and MongoDB',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/hbfawaz112/Twitter-Clone', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'moodle.png',
+    title: 'Moodle ULFS',
+    info:
+      'This is a web project developed using php-mysql , it’s about 2 website one for the instructors to upload files as pdf courses, sessions, … , and one for the students that can acces to their account and download these files.',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/hbfawaz112/moodle-UL-Faculity-Of-Science', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'FORM-OF-DISCUSION.png',
+    title: 'UL-FS Form Of Discussion',
+    info:
+      'A website developed using ASP.NET MVC to let students communicate between them by asking public questions or private questions based on many factors , and reply to these questions',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/hbfawaz112/UL-FS-Form-of-Discussion-ASP.NET-MVC', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'football-managment-systems.png',
+    title: 'Football Academies Managment System',
+    info:
+      'A Desktop app developed with java (GUI-swing) for managing data of a football academy (Players , Coaches , Training and the academy balance)',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/hbfawaz112/moodle-UL-Faculity-Of-Science', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +84,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'hbfawaz112@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +93,27 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/hussfwz',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'facebook',
+      url: 'https://www.facebook.com/profile.php?id=100011177776172',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/hussein-b-fawaz-21a778140/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/hbfawaz112',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
